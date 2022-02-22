aca6a29a007f54fcf72b702387b022eb72456110
eugen-bondarev <eug.bondarev@gmail.com>
2022-02-22 22:21:16 +0100

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: ColorManager.createMaterialColor(const Color.fromRGBO(
-            22, 23, 52, 1.0)),
+        primarySwatch: ColorManager.createMaterialColor(
+            const Color.fromRGBO(22, 23, 52, 1.0)),
       ),
       home: const MyHomePage(title: 'Small Wikipedia Browser'),
     );
@@ -72,15 +72,12 @@ class Article {
   }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String label = "";
+class _ArticleWidget {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String label = "";
 
   final textFieldController = TextEditingController();
 
@@ -93,24 +90,23 @@ class _MyHomePageState extends State<MyHomePage> {
   late Article currentArticle = Article("", "");
 
   void fetchArticle(String title) async {
-    // print(query);
-    String query = sprintf('https://de.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&prop=extracts&explaintext=True&titles=%s', [title.replaceAll(" ", "_")]);
-    // print(query);
-    final response = await http
-        .get(Uri.parse(query));
+    String query = sprintf(
+        'https://de.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&prop=extracts&explaintext=True&titles=%s',
+        [title.replaceAll(" ", "_")]);
+    final response = await http.get(Uri.parse(query));
 
     if (response.statusCode == 200) {
-      var json = jsonDecode(response.body);
-      var responseQuery = json['query'];
-      var responsePages = responseQuery['pages'] as Map;
-      var firstKey = responsePages.keys.elementAt(0);
-
+      Map<String, dynamic> json =
+          jsonDecode(response.body) as Map<String, dynamic>;
+      Map<String, dynamic> responseQuery = json['query'];
+      Map<String, dynamic> responsePages = responseQuery['pages'];
+      String firstKey = responsePages.keys.elementAt(0);
       String title = responsePages[firstKey]['title'].toString();
       String content = responsePages[firstKey]['extract'].toString();
 
-      currentArticle.unload();
-      currentArticle = Article(title, content);
-      setState(() {});
+      setState(() {
+        currentArticle = Article(title, content);
+      });
     } else {
       throw Exception('Failed to load album');
     }
@@ -120,6 +116,22 @@ class _MyHomePageState extends State<MyHomePage> {
     fetchArticle(label);
   }
 
+  List<Widget> getFoo() {
+    if (currentArticle.isLoaded()) {
+      return [
+        Text(
+          currentArticle.title,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        Expanded(
+            child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Text(currentArticle.content)))
+      ];
+    }
+    return [];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -127,64 +139,39 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+          child: Padding(
+        padding: const EdgeInsets.only(left: 8, top: 8, right: 8, bottom: 0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
                     flex: 3,
-                    child:
-                    SizedBox(
-                      height: 80,
-                      child:
-                        TextField(
+                    child: SizedBox(
+                        height: 80,
+                        child: TextField(
                             controller: textFieldController,
                             onChanged: onTextFieldChange,
                             decoration: const InputDecoration(
                                 border: OutlineInputBorder(),
-                                hintText: "Titel eines Wikipedia-Artikels"
-                            )
-                        )
-                    )
-                  ),
-                  Expanded(
+                                hintText: "Titel eines Wikipedia-Artikels")))),
+                Expanded(
                     flex: 1,
-                    child:
-                      SizedBox(
+                    child: SizedBox(
                         height: 60,
-                        child:
-                          ElevatedButton(
-                            onPressed: onSearchButtonPressed,
-                            child: const Text('Suche'),
-                          )
-                      )
-                  )
-                ],
-              )
+                        child: ElevatedButton(
+                          onPressed: onSearchButtonPressed,
+                          child: const Text('Suche'),
+                        )))
+              ],
             ),
-              if (currentArticle != null && currentArticle.isLoaded()) ...[
-              Text(
-                currentArticle.title,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child:
-                    Text(
-                      currentArticle.content
-                    )
-                )
-              )
-            ]
+            ...getFoo()
           ],
         ),
-      ),
+      )),
     );
   }
 }
